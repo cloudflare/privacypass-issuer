7faf0faec708ccbae9c39972253b9f84ec8b4672
Thibault Meunier <thibault@cloudflare.com>
2023-11-03 13:27:06 +0000

    Add key rotation support
    Issuer key can be rotated to minimise attack surface. This implies that
    two keys might live at the same time, and that keys might have to be
    cleaned after some time.
    
    This commit adds support for keuy rotation. It does so by adding a daily
    cron trigger. On the first of the month, a new key is added. Every day
    which is not the first, keys are cleared so only the latest one
    persists.
    A new admin endpoint is also included /admin/clear to manually trigger
    the clearing of keys.
    
    Finally, the test suite is updated to reflect the underlying change in
    key indexation, which now rely on truncated token id.

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,11 +1,23 @@
 import { Context } from '../src/context';
 import { handleTokenRequest, default as workerObject } from '../src/index';
+import { IssuerConfigurationResponse } from '../src/types';
 import { b64ToB64URL, u8ToB64 } from '../src/utils/base64';
 import { ExecutionContextMock, getContext, getEnv } from './mocks';
 import * as blindrsa from '@cloudflare/blindrsa-ts';
-import { TokenRequest, util } from '@cloudflare/privacypass-ts';
+import {
+	MediaType,
+	PRIVATE_TOKEN_ISSUER_DIRECTORY,
+	TokenRequest,
+	util,
+} from '@cloudflare/privacypass-ts';
 
 const sampleURL = 'http://localhost';
+
+const keyToTokenKeyID = async (key: Uint8Array): Promise<number> => {
+	const hash = await crypto.subtle.digest('SHA-256', key);
+	const u8 = new Uint8Array(hash);
+	return u8[u8.length - 1];
+};
 
 describe('challenge handlers', () => {
 	const suite = blindrsa.SUITES.SHA384.PSS.Deterministic();
@@ -24,8 +36,9 @@ describe('challenge handlers', () => {
 			(await crypto.subtle.exportKey('spki', keypair.publicKey)) as ArrayBuffer
 		);
 		const publicKeyEnc = b64ToB64URL(u8ToB64(util.convertEncToRSASSAPSS(publicKey)));
+		const tokenKey = await keyToTokenKeyID(new TextEncoder().encode(publicKeyEnc));
 		await ctx.env.ISSUANCE_KEYS.put(
-			'latest',
+			tokenKey.toString(),
 			(await crypto.subtle.exportKey('pkcs8', keypair.privateKey)) as ArrayBuffer,
 			{ customMetadata: { publicKey: publicKeyEnc } }
 		);
@@ -45,24 +58,21 @@ describe('challenge handlers', () => {
 		);
 		const publicKeyU8 = util.convertEncToRSASSAPSS(publicKeyExport);
 		const publicKeyEnc = b64ToB64URL(u8ToB64(publicKeyU8));
-		const keyId = new Uint8Array(
-			await crypto.subtle.digest('SHA-256', new TextEncoder().encode(publicKeyEnc))
-		);
-		const tokenKeyId = keyId[keyId.length - 1];
+		const tokenKeyId = await keyToTokenKeyID(new TextEncoder().encode(publicKeyEnc));
 
 		// note that blindedMsg should be the payload and not the message directly
 		const tokenRequest = new TokenRequest(tokenKeyId, blindedMsg);
 
 		const request = new Request(tokenRequestURL, {
 			method: 'POST',
-			headers: { 'content-type': 'application/private-token-request' },
+			headers: { 'content-type': MediaType.PRIVATE_TOKEN_REQUEST },
 			body: tokenRequest.serialize(),
 		});
 
 		const response = await handleTokenRequest(ctx, request);
 
 		expect(response.status).toBe(200);
-		expect(response.headers.get('content-type')).toBe('application/private-token-response');
+		expect(response.headers.get('content-type')).toBe(MediaType.PRIVATE_TOKEN_RESPONSE);
 
 		const blindSignature = new Uint8Array(await response.arrayBuffer());
 		const signature = await suite.finalize(publicKey, preparedMsg, blindSignature, inv);
@@ -86,5 +96,53 @@ describe('non existing handler', () => {
 		const response = await workerObject.fetch(request, getEnv(), new ExecutionContextMock());
 
 		expect(response.status).toBe(404);
+	});
+});
+
+describe('rotate and clear key', () => {
+	const rotateURL = `${sampleURL}/admin/rotate`;
+	const clearURL = `${sampleURL}/admin/clear`;
+	const directoryURL = `${sampleURL}${PRIVATE_TOKEN_ISSUER_DIRECTORY}`;
+
+	it('should generate multiple keys', async () => {
+		const rotateRequest = new Request(rotateURL, { method: 'POST' });
+		const directoryRequest = new Request(directoryURL);
+
+		const NUMBER_OF_KEYS_GENERATED = 3;
+		for (let i = 0; i < NUMBER_OF_KEYS_GENERATED; i += 1) {
+			await workerObject.fetch(rotateRequest, getEnv(), new ExecutionContextMock());
+		}
+
+		const response = await workerObject.fetch(
+			directoryRequest,
+			getEnv(),
+			new ExecutionContextMock()
+		);
+		expect(response.ok).toBe(true);
+
+		const directory: IssuerConfigurationResponse = await response.json();
+		expect(directory['token-keys']).toHaveLength(NUMBER_OF_KEYS_GENERATED);
+	});
+
+	it('should clear keys and keep only one', async () => {
+		const rotateRequest = new Request(rotateURL, { method: 'POST' });
+		const clearRequest = new Request(clearURL, { method: 'POST' });
+		const directoryRequest = new Request(directoryURL);
+
+		const NUMBER_OF_KEYS_GENERATED = 3;
+		for (let i = 0; i < NUMBER_OF_KEYS_GENERATED; i += 1) {
+			await workerObject.fetch(rotateRequest, getEnv(), new ExecutionContextMock());
+		}
+		await workerObject.fetch(clearRequest, getEnv(), new ExecutionContextMock());
+
+		const response = await workerObject.fetch(
+			directoryRequest,
+			getEnv(),
+			new ExecutionContextMock()
+		);
+		expect(response.ok).toBe(true);
+
+		const directory: IssuerConfigurationResponse = await response.json();
+		expect(directory['token-keys']).toHaveLength(1);
 	});
 });
