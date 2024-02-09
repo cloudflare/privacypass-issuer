a1d57aadcd274bf51c2b76fc99738da92b038553
Thibault Meunier <thibault@cloudflare.com>
2024-02-09 10:17:08 +0100

    Add cache for issuer directory
    Insatead of computing the issuer directory whenever requested, cache it
    with the same setting used by clients.
    This cache is clearer upon key rotation.
    This commit also allows cache time to be configured per deployment.

--- src/bindings.ts
+++ src/bindings.ts
@@ -2,6 +2,7 @@ import type { R2Bucket } from '@cloudflare/workers-types/2023-07-01';
 
 export interface Bindings {
 	// variables and secrets
+	DIRECTORY_CACHE_MAX_AGE_SECONDS: string;
 	ENVIRONMENT: string;
 	LOGGING_SHIM_TOKEN: string;
 	SENTRY_ACCESS_CLIENT_ID: string;
