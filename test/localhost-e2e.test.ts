fab155f47cb14a73678f099a46de40afbc5649a8
armfazh <armfazh@cloudflare.com>
2024-06-19 16:32:36 -0700

    Fixing spawn process.

--- test/localhost-e2e.test.ts
+++ test/localhost-e2e.test.ts
@@ -1,43 +1,47 @@
-import { spawn, ChildProcess } from 'child_process';
+import { spawn } from 'node:child_process';
 import { testE2E } from './e2e/issuer';
 
 const ISSUER_URL = 'localhost:8787';
 
 describe('e2e on localhost', () => {
-	let serverProcess: ChildProcess;
+	let serverPID: number | undefined;
 
-	beforeEach(() => {
-		// start server as an independant process with npm run dev
-		serverProcess = spawn('npm', ['run', 'dev']);
-	});
+	beforeAll(async () => {
+		try {
+			// start server as an independant process with npm run dev
+			const serverProcess = spawn('npm', ['run', 'dev'], { stdio: 'inherit', detached: true });
+			serverPID = serverProcess.pid;
+			console.log('Creating server with PID: ', serverPID);
 
-	afterEach(() => {
-		serverProcess?.kill();
-	});
-
-	it(
-		'should issue a token that is valid',
-		async () => {
 			// check the server is online
 			const backoffInMs = 100;
 			while (true) {
 				try {
 					await fetch(`http://${ISSUER_URL}/`);
+					console.log('Server is up');
 					break;
 				} catch (e) {
 					await new Promise(resolve => setTimeout(resolve, backoffInMs));
 				}
 			}
+		} catch (err) {
+			console.log('Server failure: ', err);
+		}
+	}, 10 * 1000);
 
-			// provision new keys
-			const response = await fetch(`http://${ISSUER_URL}/admin/rotate`, {
-				method: 'POST',
-			});
-			expect(response.ok).toBe(true);
+	afterAll(() => {
+		process.kill(-serverPID!);
+		console.log('Server is down');
+	});
 
-			const e2e = await testE2E(ISSUER_URL);
-			expect(e2e).toBe(true);
-		},
-		10 * 1000
-	);
+	it('should issue a token that is valid', async () => {
+		// provision new keys
+		const response = await fetch(`http://${ISSUER_URL}/admin/rotate`, {
+			method: 'POST',
+		});
+		expect(response.ok).toBe(true);
+
+		const e2e = await testE2E(ISSUER_URL);
+		expect(e2e).toBe(true);
+	});
 });
