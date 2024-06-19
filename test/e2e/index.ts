f073270ad676b5da5d925a68018a8cdfd238e4b9
Thibault Meunier <thibault@cloudflare.com>
2024-06-19 18:13:06 +0200

    Add test:e2e target
    Allow testing an issuer end-to-end from the command line.
    Locally, the client fetches the issuer public key and token request uri.
    It then generates a token request, sends it, and validates the received
    token.
    
    mTLS is supported, as it's a popular authentication method.

---
+++ test/e2e/index.ts
@@ -0,0 +1,37 @@
+import { program } from 'commander';
+import { MTLSConfiguration, testE2E } from './issuer.js';
+
+async function testCommand(issuerName: string, mTLS?: MTLSConfiguration) {
+	console.log('Testing with issuer', issuerName);
+	const validIssuer = await testE2E(issuerName, mTLS);
+
+	if (validIssuer) {
+		console.log('Issuer tokens are valid');
+	} else {
+		console.error('Issuer tokens are not valid');
+	}
+}
+
+async function main() {
+	program
+		.option('--cert <path>', 'Path to client certificate. e.g. ./client.crt')
+		.option('--key <path>', 'Path to client key. e.g. ./client.key')
+		.argument('<issuer-name>', 'Name of the issuer. e.g. demo-pat.issuer.cloudflare.com')
+		.action(async (issuerName, options) => {
+			try {
+				if (options.cert && options.key) {
+					await testCommand(issuerName, { certPath: options.cert, keyPath: options.key });
+				} else if (options.cert || options.key) {
+					console.error('You must specify both --cert and --key');
+				} else {
+					await testCommand(issuerName);
+				}
+			} catch (e) {
+				console.error(e);
+			}
+		});
+
+	program.parse();
+}
+
+main();
