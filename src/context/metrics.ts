b615d3e16cba2d2459a2167304f4020234a77fa2
Lina Baquero <lina@cloudflare.com>
2024-06-20 12:03:11 -0400

    Add metric to track how many times each key is used/add logic for havinf default env vars

--- src/context/metrics.ts
+++ src/context/metrics.ts
@@ -1,17 +1,25 @@
-import { CounterType, RegistryType } from 'promjs';
+import { CounterType, Labels, RegistryType } from 'promjs';
 import { Registry } from 'promjs/registry';
+import { Bindings } from '../bindings';
 export const METRICS_ENDPOINT = 'https://workers-logging.cfdata.org/prometheus';
 
 export interface RegistryOptions {
 	bearerToken?: string;
 }
 
+export interface DefaultLabels {
+	env: string;
+	service: string;
+}
+
 /**
  * A wrapper around the promjs registry to manage registering and publishing metrics
  */
 export class MetricsRegistry {
-	registry: RegistryType;
+	env: Bindings;
+
 	options: RegistryOptions;
+	registry: RegistryType;
 
 	directoryCacheMissTotal: CounterType;
 	erroredRequestsTotal: CounterType;
@@ -22,46 +30,65 @@ export class MetricsRegistry {
 	signedTokenTotal: CounterType;
 	r2RequestsTotal: CounterType;
 
-	constructor(options: RegistryOptions) {
+	constructor(env: Bindings, options: RegistryOptions) {
+		this.env = env;
 		this.options = options;
 		this.registry = new Registry();
 
-		this.directoryCacheMissTotal = this.registry.create(
+		this.directoryCacheMissTotal = this.create(
 			'counter',
 			'directory_cache_miss_total',
 			'Number of requests for private token issuer directory which are not served by the cache.'
 		);
-		this.erroredRequestsTotal = this.registry.create(
+		this.erroredRequestsTotal = this.create(
 			'counter',
 			'errored_requests_total',
 			'Errored requests served to eyeball'
 		);
-		this.issuanceRequestTotal = this.registry.create(
+		this.issuanceRequestTotal = this.create(
 			'counter',
 			'issuance_request_total',
 			'Number of requests for private token issuance.'
 		);
-		this.keyRotationTotal = this.registry.create(
+		this.keyRotationTotal = this.create(
 			'counter',
 			'key_rotation_total',
 			'Number of key rotation performed.'
 		);
-		this.keyClearTotal = this.registry.create(
+		this.keyClearTotal = this.create(
 			'counter',
 			'key_clear_total',
 			'Number of key clear performed.'
 		);
-		this.requestsTotal = this.registry.create('counter', 'requests_total', 'total requests');
-		this.signedTokenTotal = this.registry.create(
+		this.requestsTotal = this.create('counter', 'requests_total', 'total requests');
+		this.signedTokenTotal = this.create(
 			'counter',
 			'signed_token_total',
 			'Number of issued signed private tokens.'
 		);
-		this.r2RequestsTotal = this.registry.create(
-			'counter',
-			'r2_requests_total',
-			'Number of accesses to R2'
-		);
+		this.r2RequestsTotal = this.create('counter', 'r2_requests_total', 'Number of accesses to R2');
+	}
+
+	private create(type: 'counter', name: string, help?: string): CounterType {
+		const counter = this.registry.create(type, name, help);
+		const defaultLabels: DefaultLabels = { env: this.env.ENVIRONMENT, service: this.env.SERVICE };
+		return new Proxy(counter, {
+			get(target, prop, receiver) {
+				if (['collect', 'get', 'inc', 'reset'].includes(prop.toString())) {
+					return function (labels?: Labels) {
+						const mergedLabels = { ...defaultLabels, ...labels };
+						return Reflect.get(target, prop, receiver)?.call(target, mergedLabels);
+					};
+				}
+				if (['add', 'set'].includes(prop.toString())) {
+					return function (value: number, labels?: Labels) {
+						const mergedLabels = { ...defaultLabels, ...labels };
+						return Reflect.get(target, prop, receiver)?.call(target, value, mergedLabels);
+					};
+				}
+				return Reflect.get(target, prop, receiver);
+			},
+		});
 	}
 
 	/**
