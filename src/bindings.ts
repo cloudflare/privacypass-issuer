da1d6979874aaa44827c92a0178c48242508c1d1
Lina Baquero <lina@cloudflare.com>
2025-02-07 15:43:38 -0500

    commit

--- src/bindings.ts
+++ src/bindings.ts
@@ -1,7 +1,9 @@
 // Copyright (c) 2023 Cloudflare, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import { Issuer } from '@cloudflare/privacypass-ts/lib/src/priv_verif_token';
 import type { R2Bucket, Performance } from '@cloudflare/workers-types/2023-07-01';
+import { IssuerHandler } from '.';
 
 export interface Bindings {
 	// variables and secrets
@@ -32,4 +34,7 @@ export interface Bindings {
 	LOGGING_SHIM_TOKEN: string;
 	WSHIM_SOCKET?: Fetcher;
 	WSHIM_ENDPOINT: string;
+
+	// Service bindings (RCP)
+	IssuerHandler: Service<IssuerHandler>
 }
